819d5f961cb2d1dc379ef98b7cd6bfe72ccedfff
elgonzy <adriangonzalez2003@ginebro.cat>
2022-12-02 16:22:01 +0100

    Finished buttons previous and next for months

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,47 +3,67 @@
 let weekDaysNames = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
 let monthsNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 let date = new Date();
-let monthDays = [];
-let actualMonth = date.getMonth();
-let actualYear = date.getFullYear();
-
-for (let i = 1; i < new Date(date.getFullYear(), date.getMonth(), 0).getDate(); i++) {
-    monthDays[i - 1] = i;
-}
 
 function createDiv(content, col) {
 
+    let calendarDiv = document.getElementById("calendar");
     let newDiv = document.createElement("div");
     let newP = document.createElement("p");
     let newContent = document.createTextNode(content);
-    let calendarDiv;
 
     newP.appendChild(newContent);
     newDiv.appendChild(newP);
 
     newDiv.classList.add(col);
 
-    calendarDiv = document.getElementById("calendar");
-
     calendarDiv.appendChild(newDiv)
 
 }
 
-createDiv(actualYear.toString(), "year");
-createDiv("de " + monthsNames[actualMonth], "monthName");
+function printCalendar(date) {
 
-weekDaysNames.forEach(day => {
-    createDiv(day, "weekNames")
-});
+    let calendarDiv = document.getElementById("calendar");
 
-for (let i = 0; i < new Date(date.getFullYear(), date.getMonth(), 1).getDay(); i++) {
-    createDiv(" ", "week");
-}
-monthDays.forEach(day => {
-    if (date.getDate() != day) {
-        createDiv(day.toString(), "week");
-    } else {
-        createDiv(day.toString(), "actualDay");
+    calendarDiv.innerHTML = "";
 
+    createDiv(date.getFullYear().toString(), "year");
+    createDiv("de " + monthsNames[date.getMonth()], "monthName");
+
+    let monthDays = [];
+
+    for (let i = 0; i < new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(); i++) {
+        monthDays[i] = i + 1;
     }
-});
+
+    weekDaysNames.forEach(day => {
+        createDiv(day, "weekNames")
+    });
+
+    for (let i = 0; i < new Date(date.getFullYear(), date.getMonth(), 1).getDay(); i++) {
+        createDiv(" ", "week");
+    }
+    monthDays.forEach(day => {
+        if (date.getDate() != day) {
+            createDiv(day.toString(), "week");
+        } else {
+            createDiv(day.toString(), "actualDay");
+
+        }
+    });
+}
+
+function nextMonth(date) {
+
+    date.setMonth(date.getMonth() + 1);
+    printCalendar(date);
+
+}
+
+function previousMonth(date) {
+
+    date.setMonth(date.getMonth() - 1);
+    printCalendar(date);
+
+}
+
+printCalendar(date);
