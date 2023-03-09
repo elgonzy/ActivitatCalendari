afa469bc10993f243e7ad3773c7d8fce975904eb
elgonzy <adriangonzalez2003@ginebro.cat>
2023-03-09 14:01:06 +0100

    Hasta la penultima activdad todo con jquery

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,143 +1,143 @@
-"use strict"
-
-let weekDaysNames = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
-let monthsNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+"use strict";
 let date = new Date();
-let ActualMonth = date.getMonth()
+$(document).ready(function() {
+    let weekDaysNames = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    let monthsNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    let ActualMonth = date.getMonth()
 
-function createDiv(content, col) {
+    function createDiv(content, col) {
 
-    let calendarDiv = document.getElementById("calendar");
-    let newDiv = document.createElement("div");
-    let newP = document.createElement("p");
-    let newContent = document.createTextNode(content);
+        let $calendarDiv = $("#calendar");
+        let $newDiv = $("<div>").appendTo($calendarDiv);
+        let $newP = $("<p>").appendTo($newDiv);
+        let $newContent = $(document.createTextNode(content)).appendTo($newP);
 
-    newP.appendChild(newContent);
-    newDiv.appendChild(newP);
+        $newDiv.addClass(col);
 
-    newDiv.classList.add(col);
-
-    calendarDiv.appendChild(newDiv);
-
-    return newDiv;
-
-}
-
-function printDaysBefore(date) {
-
-    let fristWeekDayActualMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-    let day = (new Date(date.getFullYear(), date.getMonth(), 0).getDate() - fristWeekDayActualMonth) + 1;
-
-    for (let i = fristWeekDayActualMonth; i > 0; i--) {
-
-        createDiv(day.toString(), "week")
-        day++;
+        return $newDiv;
 
     }
 
-}
+    function printDaysBefore(date) {
 
-function printDaysAfter(date) {
+        let fristWeekDayActualMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+        let day = (new Date(date.getFullYear(), date.getMonth(), 0).getDate() - fristWeekDayActualMonth) + 1;
 
-    let lastWeekDayActualMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
-    let day = 1;
+        for (let i = fristWeekDayActualMonth; i > 0; i--) {
+            createDiv(day.toString(), "week");
+            day++;
 
-    for (let i = 7 - lastWeekDayActualMonth; i > 0; i--) {
-
-        createDiv(day.toString(), "week")
-        day++;
-
-    }
-
-}
-
-function generateKey(year, month, day) {
-
-    let key;
-
-    key = year;
-    key += month < 10 ? "0" + month : month;
-    key += day < 10 ? "0" + day : day;
-
-    return key;
-
-}
-
-function printComent(key, div) {
-
-    if (localStorage.getItem(key)) {
-
-        let content = localStorage.getItem(key);
-        div.classList.add("hasComent");
-
-        let newP = document.createElement("p");
-        let newContent = document.createTextNode(content);
-
-        newP.appendChild(newContent);
-        div.appendChild(newP);
-
-    }
-}
-
-function createComent(key, div) {
-
-    div.setAttribute("onclick", "localStorage.setItem(" + key + ", prompt('Introduce tu comentario: '))");
-
-    div.classList.add("hasComent");
-}
-
-function printCalendar(date) {
-
-    let calendarDiv = document.getElementById("calendar");
-    let fullYear = date.getFullYear();
-    let month = date.getMonth();
-
-    calendarDiv.innerHTML = "";
-
-    createDiv(fullYear.toString(), "year");
-    createDiv("de " + monthsNames[month], "monthName");
-
-    let monthDays = [];
-
-    for (let i = 0; i < new Date(fullYear, month + 1, 0).getDate(); i++) {
-        monthDays[i] = i + 1;
-    }
-
-    weekDaysNames.forEach(day => {
-        createDiv(day, "weekNames")
-    });
-
-    printDaysBefore(date);
-
-    monthDays.forEach(day => {
-        let key = generateKey(fullYear, month, day);
-        if (date.getDate() == day && ActualMonth == month) {
-            let div = createDiv(day.toString(), "actualDay");
-            createComent(key, div);
-            printComent(key, div);
-        } else {
-            let div = createDiv(day.toString(), "week");
-            createComent(key, div);
-            printComent(key, div);
         }
+
+    }
+
+    function printDaysAfter(date) {
+
+        let lastWeekDayActualMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
+        let day = 1;
+
+        for (let i = 7 - lastWeekDayActualMonth; i > 0; i--) {
+
+            createDiv(day.toString(), "week");
+            day++;
+
+        }
+
+    }
+
+    function generateKey(year, month, day) {
+
+        let key;
+
+        key = year;
+        key += month < 10 ? "0" + month : month;
+        key += day < 10 ? "0" + day : day;
+
+        return key;
+
+    }
+
+    function printComent(key, div) {
+
+        if (localStorage.getItem(key)) {
+
+            let content = localStorage.getItem(key);
+            div.addClass("hasComent");
+
+            let $newP = $("<p>").appendTo(div);
+            let $newContent = $(document.createTextNode(content)).appendTo($newP);
+
+        }
+    }
+
+    function createComent(key, div) {
+
+        div.attr("onclick", "localStorage.setItem(" + key + ", prompt('Introduce tu comentario: '))");
+
+        div.addClass("hasComent");
+    }
+
+    function printCalendar(date) {
+
+        let $calendarDiv = $("#calendar");
+        let fullYear = date.getFullYear();
+        let month = date.getMonth();
+
+        $calendarDiv.empty();
+
+        createDiv(fullYear.toString(), "year");
+        createDiv("de " + monthsNames[month], "monthName");
+
+        let monthDays = [];
+
+        for (let i = 0; i < new Date(fullYear, month + 1, 0).getDate(); i++) {
+            monthDays[i] = i + 1;
+        }
+
+        $.each(weekDaysNames, function(index, day) {
+            createDiv(day, "weekNames");
+        });
+
+        printDaysBefore(date);
+
+        $.each(monthDays, function(index, day) {
+            let key = generateKey(fullYear, month, day);
+            if (date.getDate() == day && ActualMonth == month) {
+                let $div = createDiv(day.toString(), "actualDay");
+                createComent(key, $div);
+                printComent(key, $div);
+            } else {
+                let $div = createDiv(day.toString(), "week");
+                createComent(key, $div);
+                printComent(key, $div);
+            }
+        });
+
+        printDaysAfter(date);
+
+    }
+
+    function nextMonth() {
+
+        date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+        printCalendar(date);
+
+    }
+
+    function previousMonth() {
+
+        date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+        printCalendar(date);
+
+    }
+
+    printCalendar(date);
+
+    $("#nextButton").click(function() {
+        nextMonth();
     });
 
-    printDaysAfter(date);
-
-}
-
-function nextMonth(date) {
-
-    date.setMonth(date.getMonth() + 1);
-    printCalendar(date);
-
-}
-
-function previousMonth(date) {
-
-    date.setMonth(date.getMonth() - 1);
-    printCalendar(date);
-
-}
-
-printCalendar(date);
+    $("#previousButton").click(function() {
+        previousMonth();
+    });
+});
